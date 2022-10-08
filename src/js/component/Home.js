da9415b409cf37d3b70c02ef36ead11beabaf007
Abrahamjf <abrahamjferrer@gmail.com>
2022-10-08 21:50:30 +0000

    :pencil: se agregaron estilos

--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -14,17 +14,39 @@ export const Home = () => {
 			setTask("")
 		};
 	};
+	// const eraser = (id) => {
+	// 	const newListOfTask = listOfTasks.filter((e,index)=> index != id);
+	// 	setlistOfTasks(newListOfTask);
+	// };
 
+	// let eraseTask = () =>{
+	// 	listOfTasks.eraser(task);
+	// }
+	let eraseTask = () => {
+		const newTasks = listOfTasks.filter((task, index) => {
+			if (id === index) {
+				return false;
+			}
+			return true;
+		})
+	};
 
+	const count = {};
+
+	for (task of listOfTasks) {
+		if (count [task]) {
+
+		}
+	}
 
 	return (
 		<React.Fragment>
 			<Title />
 			<div className="box d-flex flex-column">
             	<input onChange={evento => setTask(evento.target.value)} onKeyDown={handlerKey} value={task} className="tasks"placeholder="What needs to be done?"></input>
-				{listOfTasks && listOfTasks.map((task,index)=> <div key={index}>{task}</div>)}
-				{listOfTasks && listOfTasks.length == 0 && <h2>No tasks, add a task.</h2>}
-
+				{listOfTasks && listOfTasks.map((task,index)=> <span key={index} classsName="alltask">{task} <button onClick={eraseTask}>X</button></span>)}
+				{listOfTasks && listOfTasks.length == 0 && <h2 className="list">No tasks, add a task.</h2>}
+				
         	</div>
 		</React.Fragment>
 	);
--- src/js/component/Title.js
+++ src/js/component/Title.js
@@ -4,7 +4,7 @@ import React from "react";
 export const Title = () => {
     return (
         <div className ="container">
-            <h1 className ="my-title">todos</h1>
+            <h1 className ="my-title">Todos'</h1>
         </div>
     );
 };
