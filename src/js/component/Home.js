5d100169d30123947cc52501091b47880a7e1637
Abrahamjf <abrahamjferrer@gmail.com>
2022-10-17 19:53:06 +0000

    :hammer: se comenzo a trabajar con la api

--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -1,61 +1,83 @@
-import React, {useState} from "react";
-import {Title} from "./Title.js";
-import {Input} from "./Input.js";
+import React, { useState, useEffect } from "react";
+import { Title } from "./Title.js";
+import { Input } from "./Input.js";
 
+let initialState = {
+  label: "",
+  done: false,
+};
 
+let urlBase = "https://assets.breatheco.de/apis/fake/todos/user/abrahamjf"
 
 //create your first component
 export const Home = () => {
-	const [task, setTask] = useState("")
-	const [listOfTasks, setlistOfTasks] = useState([])
-	let handlerKey = (evento) => {
-		if(evento.key == "Enter" && task != ""){
-			setlistOfTasks([...listOfTasks, task])
-			setTask("")
-		};
-	};
-	// const eraser = (id) => {
-	// 	const newListOfTask = listOfTasks.filter((e,index)=> index != id);
-	// 	setlistOfTasks(newListOfTask);
-	// };
+  const [task, setTask] = useState(initialState);
+  const [listOfTasks, setlistOfTasks] = useState([]);
+  let handlerKey = (event) => {
+    if (event.key == "Enter" && task != "") {
+      setlistOfTasks([...listOfTasks, task]);
+      setTask("");
+    }
+  };
 
-	// let eraseTask = () =>{
-	// 	listOfTasks.eraser(task);
-	// }
-	// let eraseTask = () => {
-	// 	const newTasks = listOfTasks.filter((task, index) => {
-	// 		if (id === index) {
-	// 			return false;
-	// 		}
-	// 		return true;
-	// 	})
-	// };
+  let handleChange = (event) => {
+	setTask({...task,[event.target.name]:event.target.value})
+  }
 
-	// const count = {};
-
-	// for (task of listOfTasks) {
-	// 	if (count [task]) {
-
-	// 	}
-	// }
-
-	return (
-		<React.Fragment>
-			<Title />
-			<div className="box d-flex flex-column">
-            	<input onChange={evento => setTask(evento.target.value)} onKeyDown={handlerKey} value={task} className="tasks"placeholder="What needs to be done?"></input>
-				{listOfTasks && listOfTasks.map((task,index)=> <span key={index} classsName="alltask">{task} <button onClick={(event) => setlistOfTasks(listOfTasks.filter((element, id)=>{ return index !== id;}))}>X</button></span>)}
-				{listOfTasks && listOfTasks.length == 0 && <h2 className="list">No tasks, add a task.</h2>}
-				{
-					<span className="counter d-flex">
-						{""}
-						{listOfTasks.length === 0
-							? "no task"
-							: listOfTasks.length + " item left"}{""}
-					</span>
-				}
-        	</div>
-		</React.Fragment>
-	);
+  let getTask = async() => {
+	try{
+		let response = await fetch(urlBase)
+		let data = await response.json()
+		if (response.ok) {
+			setlistOfTasks(data)
+		} else {
+			console.log("Debo crear el usuario")
+		}
+	}catch(error){console.log(error)}
+  }
+  useEffect(()=>{getTask()},[])
+  return (
+    <React.Fragment>
+      <Title />
+      <div className="box d-flex flex-column">
+        <input
+          onChange={handleChange}
+          onKeyDown={handlerKey}
+          value={task.label}
+		  name="label"
+          className="tasks"
+          placeholder="What needs to be done?"
+        ></input>
+        {listOfTasks &&
+          listOfTasks.map((task, index) => (
+            <span key={index} className="alltask">
+              {task.label}
+              <button
+                onClick={(event) =>
+                  setlistOfTasks(
+                    listOfTasks.filter((element, id) => {
+                      return index !== id;
+                    })
+                  )
+                }
+              >
+                X
+              </button>
+            </span>
+          ))}
+        {listOfTasks && listOfTasks.length == 0 && (
+          <h2 className="list">No tasks, add a task.</h2>
+        )}
+        {
+          <span className="counter d-flex">
+            {""}
+            {listOfTasks.length === 0
+              ? "no task"
+              : listOfTasks.length + " item left"}
+            {""}
+          </span>
+        }
+      </div>
+    </React.Fragment>
+  );
 };
-
